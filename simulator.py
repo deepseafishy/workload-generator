1f3cc825e843f5696431839cce9419b5ea451465
deepseafishy <48512254+deepseafishy@users.noreply.github.com>
2021-07-07 16:10:12 +0900

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -12,6 +12,7 @@ import random
 import argparse
 import multiprocessing
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 
 def get_cpu_util(pid):
@@ -24,11 +25,24 @@ def get_mem_util(pid):
     output = process.stdout.readlines()
     return float(output[1])
 
-    
+def drawGraph(times, col, f_name, avg_cpu=None, avg_mem=None):
+    assert(sorted(times) == times)
+
+    if avg_cpu != None:
+        plt.figure(figsize=(15, 6), dpi=80)
+        plt.plot((times), avg_cpu, color=col)
+        plt.title(f_name)
+        plt.savefig(f_name + '.png')
+    if avg_mem != None:
+        plt.figure(figsize=(15, 6), dpi=80)
+        plt.plot((times), avg_mem, color=col)
+        plt.title(f_name + " avg_mem")
+        plt.savefig(f_name + '.png')
+
 def run_command(command, period):
     args = shlex.split(command)
     start = time()
-    process = subprocess.Popen(shlex.split(command), encoding='utf-8', stdout=subprocess.PIPE) 
+    process = subprocess.Popen(shlex.split(command), encoding='utf-8', stdout=subprocess.PIPE)
     cnt = 0
     cpu_util = 0
     mem_util = 0
@@ -54,11 +68,11 @@ def run_command(command, period):
         else:
             print('Error caused by signal ' + str(-process.poll()))
             break
-    # error 
+    # error
     process.kill()
     return -1, -1;
-    
-    
+
+
 def simulate_trace(trace_file, accelerated, core):
     '''
     SIMULATE WORKLOAD
@@ -66,24 +80,26 @@ def simulate_trace(trace_file, accelerated, core):
         - trace_file (string): name of workload to be simulated
         - accelerated (boolean): specify whether simulation will start from absolute time 0 or time[0] specified in the workload
             Ex: The workload that we want to reproduce could start from time 100. This would mean that this workload didn't exist during the first 100 cycles. accelerated = False will simulate even this part. This would be useful if you would want to turn this generator on for multiple tasks that start at different time cycles, all at the creation of a VM. accelerated = True will skip this absence of activity and assume that the generator itself is turned on at the correct moment (times[i] = 100 means that right now 100th cycle is being executed).
-     output: 
+     output:
          Nothing
     '''
     # GET NECESSARY DATA (TIME, AVG_CPU, AVG_MEM) FROM FILE
     open_file = open(trace_file)
     file = json.load(open_file)
     times = file['time']
+    times = file['time'][:12]
+    #times = file['time'][:4]
     avg_cpu = file['avg_cpu']
     avg_mem = file['avg_mem']
     mock_avg_cpu = []
     mock_avg_mem = []
-    
+
     # False: assume generator is turned on at absolute time 0
     # True: assume generator is turned on at times[0]
     if accelerated is False:
         for i in range(0, times[0]+1):
             sleep(PERIOD)
-    
+
     # Specify the portion [start, end) of the workload that would be simulated
     start = 0
     end = len(times)
@@ -98,55 +114,65 @@ def simulate_trace(trace_file, accelerated, core):
             continue;
         if it == end:
             break;
-        
+
         # CPU, MEM usage that we want to simulate
         target_cpu = avg_cpu[it]
         target_mem = avg_mem[it]
-        
-        
+
+
         # If you don't want to simulate either of CPU or MEM, comment out appropriately:
+        # command = './fake-workload/build/workload/workload'
         command = './fake-workload/build/workload/workload'
-        command = command + ' --bucket 1'
+        # command = command + ' --bucket 50'
         command = command + ' --thread ' + str(core)
+        # command = command + ' --dirty-size 1000'
+        # command = command + ' --working-set-size 10'
+        # command = command + ' --pagesize 40960'
         if target_cpu - ALPHA > ALPHA:
-            command = command + ' --cpu-util ' + str(target_cpu-ALPHA) 
-       
-#        command = command + ' --memory-size ' + str(int(target_mem*MACHINE_SIZE/1000000))
+            command = command + ' --cpu-util ' + str(target_cpu-ALPHA)
+
+        command = command + ' --memory-size ' + str(int(target_mem*MACHINE_SIZE/1000000))
         print(command)
-        
+
         # Run the resource-usage-generator
         cpu, mem = run_command(command, PERIOD)
         if(cpu < 0 or mem < 0):
             print('Error...')
             break;
-        print(100*target_cpu, cpu, target_mem, mem)
-        
+        # print(100*target_cpu, cpu, target_mem, mem)
+        print(100*target_cpu, cpu, 100*target_mem, mem)
+
         # Actual utilization returned and recorded
         mock_avg_cpu.append(cpu)
         mock_avg_mem.append(mem)
         # Calculate error
         cpu_err_margin += abs(cpu-target_cpu*100)
-        mem_err_margin += abs(mem-target_mem)
-        
-        
-  
+        # mem_err_margin += abs(mem-target_mem)
+        mem_err_margin += abs(mem-target_mem*100)
+
     print('mock: ', len(mock_avg_cpu), len(mock_avg_mem))
     print('Error margin (CPU): ', cpu_err_margin, len(mock_avg_cpu), cpu_err_margin/len(mock_avg_cpu))
     print('Error margin: (MEM)', mem_err_margin, len(mock_avg_mem), mem_err_margin/len(mock_avg_mem))
-    return times, avg_cpu, mock_avg_cpu, avg_mem, mock_avg_mem, cpu_err_margin/len(mock_avg_cpu), mem_err_margin/len(mock_avg_mem);
+    return times, [cpus * 100 for cpus in avg_cpu], mock_avg_cpu, [mems * 100 for mems in avg_mem], mock_avg_mem, cpu_err_margin/len(mock_avg_cpu), mem_err_margin/len(mock_avg_mem);
 
+def drawCPU(true_cpu, mock_cpu, times, start, end, file_name):
+    times = times[start:end]
+    plt.figure(figsize=(15, 6), dpi=80)
+    plt.plot((times), true_cpu[start:end], color='b', label='true_cpu')
+    plt.plot((times), mock_cpu, color='r', label='mock_cpu')
+    plt.title(file_name)
+    plt.legend()
+    plt.savefig(file_name + '_cpu.png')
 
-def drawCPU(true_cpu, mock_cpu, times, start, end, file_name, txt=None):
-    drawGraph(times[start:end], avg_cpu=true_cpu[start:end], col=COLOR[TYPE], f_name='{}_cpu_true'.format(file_name), textstr=txt);
-    drawGraph(times[start:end], avg_cpu=mock_cpu, col=COLOR[TYPE], f_name='{}_cpu_mock'.format(file_name), textstr=txt);
+def drawMEM(true_mem, mock_mem, times, start, end, file_name):
+    times = times[start:end]
+    plt.figure(figsize=(15, 6), dpi=80)
+    plt.plot((times), true_mem[start:end], color='b', label='true_mem')
+    plt.plot((times), mock_mem, color='r', label='mock_mem')
+    plt.title(file_name)
+    plt.legend()
+    plt.savefig(file_name + '_mem.png')
 
-    
-def drawMEM(true_mem, mock_mem, times, start, end, TRACE_FILE):
-    drawGraph(times[start:end], avg_mem=true_mem[start:end], col=COLOR[TYPE], f_name='{}_mem_true'.format(file_name));
-    drawGraph(times[start:end], avg_mem=mock_mem, col=COLOR[TYPE], f_name='{}_mem_mock'.format(file_name));
-
-    
-    
 def find_file_to_simulate(seed):
     '''
     FIND A TASK FROM 4_PROD WITH MORE THAN 30 ENTRIES
@@ -156,15 +182,13 @@ def find_file_to_simulate(seed):
     length = len(dir_list)
     n = random.randint(0, length-1)
     return dir_list[n]
-    
 
-        
-def main(seed, core):
+def main(seed, core, machine_size):
     print('seed:', "current_time" if seed==None else seed, "|", "core:", core)
     # SET PARAMETERS
     global PERIOD
     global ALPHA
-    PERIOD = 5     # seconds
+    PERIOD = 300   # seconds
     ALPHA = 1/100   # percentage of cpu the ./build process takes at mininum / 100 (to make it between [0, 1]
 
     global COLOR
@@ -183,27 +207,30 @@ def main(seed, core):
     global OUTPUT_DIR
     global TRACE_FILE_NAME
 
-    MACHINE_SIZE = 32082000000 # B
-    INPUT_DIR = './WorkloadExamples/' # Must end with /
-    OUTPUT_DIR = './WorkloadResultExamples/' 
+    MACHINE_SIZE = int(machine_size) # Bytes
+    # INPUT_DIR = './WorkloadExamples/' # Must end with /
+    INPUT_DIR = './WorkloadExamples/cpu_intensive/' # Must end with /
+    # INPUT_DIR = './WorkloadExamples/mem_intensive/' # Must end with /
+    OUTPUT_DIR = './WorkloadResultExamples/'
 
     # FIND FILE TO SIMULATE (OPTIONAL)
     TRACE_FILE_NAME = find_file_to_simulate(seed)
-    TRACE_FILE_NAME = 'file_169637062408_212549600003_3324'
+    # TRACE_FILE_NAME = 'file_169637062408_212549600003_3324'
     print(TRACE_FILE_NAME)
 
     # SIMULATE TRACE
     times, avg_cpu, mock_avg_cpu, avg_mem, mock_avg_mem, cpu_err, mem_err = simulate_trace(INPUT_DIR + TRACE_FILE_NAME, True, core)
 
     # DRAW GRAPH (OPTIONAL)
-    text = "Period {}, cpu err {}, mem_err {}".format(PERIOD, cpu_err, mem_err) 
-    drawCPU(avg_cpu, mock_avg_cpu, times, 0, len(times), TRACE_FILE_NAME+"__thrid", text);
-#    drawMEM(avg_mem, mock_avg_mem, times, 0, len(times), TRACE_FILE_NAME);
-    
+    text = "Period {}, cpu err {}, mem_err {}".format(PERIOD, cpu_err, mem_err)
+    drawCPU(avg_cpu, mock_avg_cpu, times, 0, len(times), TRACE_FILE_NAME);
+    drawMEM(avg_mem, mock_avg_mem, times, 0, len(times), TRACE_FILE_NAME);
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--core_count", default=multiprocessing.cpu_count(), help="Specify the number of cores(threads) in which the simulator will work. Default = number of cpu cores.", type=int)
     parser.add_argument("-s", "--seed", default=None, help="Specify the seed for generating a random number used in selecting the trace file. Default = current time.")
+    parser.add_argument("-m", "--machine_size", default=32000000000, type=int)
     args = parser.parse_args()
-    main(args.seed, args.core_count)
-    
+    main(args.seed, args.core_count, args.machine_size)
+
